79bc452bb71ed810926e87d9ce263a16dad8c2a1
OnsChebi <chebbions7@gmail.com>
2024-05-23 09:15:21 +0100

    fix dashboard

--- UniversityManagement/Controllers/DashboardController.cs
+++ UniversityManagement/Controllers/DashboardController.cs
@@ -22,7 +22,7 @@ namespace UniversityManagement.Controllers
 
             List<Person> SelectedPersons = await _context.Persons
                 .Include(x => x.Category)
-                .Where(y => y.BirthDate >= StartDate && y.BirthDate <= EndDate)
+               
                 .ToListAsync();
 
             //Total Students
@@ -53,43 +53,7 @@ namespace UniversityManagement.Controllers
                 .ToList();
             ViewBag.DoughnutChartData = doughnutChartData;
 
-            // Spline Chart - Students vs Employees
-            var studentsSummary = SelectedPersons
-                .Where(i => i.Category.Type == "Students")
-                .GroupBy(j => j.BirthDate)
-                .Select(k => new SplineChartData
-                {
-                    Day = k.First().BirthDate.ToString("dd-MMM"),
-                    Students = k.Count(),
-                })
-                .ToList();
-
-            var employeesSummary = SelectedPersons
-                .Where(i => i.Category.Type == "Employees")
-                .GroupBy(j => j.BirthDate)
-                .Select(k => new SplineChartData
-                {
-                    Day = k.First().BirthDate.ToString("dd-MMM"),
-                    Employee = k.Count(),
-                })
-                .ToList();
-
-            var last7Days = Enumerable.Range(0, 7)
-                .Select(i => StartDate.AddDays(i).ToString("dd-MMM"))
-                .ToArray();
-
-            var splineChartData = from day in last7Days
-                                  join students in studentsSummary on day equals students.Day into dayStudentsJoined
-                                  from students in dayStudentsJoined.DefaultIfEmpty()
-                                  join employees in employeesSummary on day equals employees.Day into dayEmployeesJoined
-                                  from employees in dayEmployeesJoined.DefaultIfEmpty()
-                                  select new SplineChartData
-                                  {
-                                      Day = day,
-                                      Students = students?.Students ?? 0,
-                                      Employee = employees?.Employee ?? 0,
-                                  };
-            ViewBag.SplineChartData = splineChartData.ToList();
+           
 
             // Recent Persons
             var recentPersons = await _context.Persons
